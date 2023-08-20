bf13780113526aada67d61cebf5012616f92417b
Kim Chi <107566226+nkchidev@users.noreply.github.com>
2023-08-20 21:11:32 +0700

    add package cors

--- src/server.js
+++ src/server.js
@@ -3,11 +3,12 @@ import bodyParser from "body-parser";
 import viewEngin from "./config/viewEngine";
 import initWebRoutes from './route/web';
 import connectDB from './config/connectDB'
+import cors from 'cors';
 
 require('dotenv').config();
 
 let app = express();
-
+app.use(cors({ credentials: true, origin: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
