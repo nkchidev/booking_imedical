06ee2f5bfceebb495b4fb84ebd02f6662fb1f036
Admin <Admin@HIEU>
2023-11-25 01:48:30 +0700

    #70

--- src/services/DoctorService.js
+++ src/services/DoctorService.js
@@ -49,19 +49,32 @@ let getAllDoctors = () => {
 let saveDetailInfoDoctor = (data) => {
     return new Promise(async (resolve, reject) => {
         try {
-            if(!data.doctorId || !data.contentHTML || !data.contentMarkdown){
+            if(!data.doctorId || !data.contentHTML || !data.contentMarkdown || !data.action) {
                 resolve({
                     errCode: 1,
                     errMessage: 'Missing parameter'
                 });
             }else{
-                await db.Markdown.create({
-                    contentHTML: data.contentHTML,
-                    contentMarkdown: data.contentMarkdown,
-                    description: data.description,
-                    doctorId: data.doctorId
-                })
-    
+                if (data.action === 'CREATE'){
+                    await db.Markdown.create({
+                        contentHTML: data.contentHTML,
+                        contentMarkdown: data.contentMarkdown,
+                        description: data.description,
+                        doctorId: data.doctorId
+                    })
+                }else if (data.action === 'EDIT'){
+                    let doctorMarkdown = await db.Markdown.findOne({
+                        where: { doctorId: data.doctorId},
+                        raw: false
+                    })
+                    
+                    if(doctorMarkdown){
+                        doctorMarkdown.contentHTML= data.contentHTML;
+                        doctorMarkdown.contentMarkdown= data.contentMarkdown;
+                        doctorMarkdown.description= data.description;
+                        await doctorMarkdown.save()
+                    }
+                }
                 resolve({
                     errCode: 0,
                     errMessage: 'Save info doctor succeed!'
